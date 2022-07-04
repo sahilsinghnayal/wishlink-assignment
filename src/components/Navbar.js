5666f590afa0811f8769d3d5a93da26287da9eff
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-07-04 16:16:51 +0530

    navbar done

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import left from "../Images/left.png";
+import profile from "../Images/profile.png";
+import verified from "../Images/verified.png"
+
+import "../App.css";
+function Navbar() {
+  return (
+    <div className="nav">
+      <button className="navbtn">
+        <img src={left} alt="leftbtn" />
+      </button>
+      <div className="profileimg">
+        <img src={profile} alt="profileimg" />
+      </div>
+      <div className="navname"> <p>Maiteryi dutta <img src={verified} alt="" /></p> </div>
+    
+      <i class="fa-solid fa-magnifying-glass"></i>
+        
+    </div>
+  );
+}
+
+export default Navbar;
