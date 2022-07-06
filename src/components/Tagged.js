1f3dd3ec4ed05ccfa2fd3d3be2e8e52a6aa0a4cf
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-07-07 00:14:15 +0530

    second section done

--- src/components/Tagged.js
+++ src/components/Tagged.js
@@ -28,7 +28,64 @@ function Tagged() {
             <button className="taggedbtn">Shop now</button>
           </div>
         </div>
+        <div className="cardoffer">  
+         <p>% Offers Available</p>
+       
+        </div>
       </div>
+      {/* complete my look //////////////////////////////////////////////// */}
+      <div className="completeContainer">
+      <div className="head1">Complete My Look</div>
+      <div className="cardcontainer">
+          <div className="taggedCard">
+            <img src={cardimg} alt="olay" />
+            <h3>Olay</h3>
+            <p>Olay white radiance creame</p>
+            <p>
+              ₹1000 <del> ₹2000</del>
+            </p>
+            <p className=" taggedoff">(50% Off)</p>
+            <button className="taggedbtn">Shop now</button>
+          </div>
+          <div className="taggedCard">
+            <img src={cardimg} alt="olay" />
+            <h3>Olay</h3>
+            <p>Olay white radiance creame</p>
+            <p>
+              ₹1000 <del> ₹2000</del>
+            </p>
+            <p className=" taggedoff">(50% Off)</p>
+            <button className="taggedbtn">Shop now</button>
+          </div>
+        </div>
+        <div className="cardoffer">  
+         <p>% Offers Available</p>
+       
+        </div>
+        <div className="cardcontainer">
+          <div className="taggedCard">
+            <img src={cardimg} alt="olay" />
+            <h3>Olay</h3>
+            <p>Olay white radiance creame</p>
+            <p>
+              ₹1000 <del> ₹2000</del>
+            </p>
+            <p className=" taggedoff">(50% Off)</p>
+            <button className="taggedbtn">Shop now</button>
+          </div>
+         
+        </div>
+      </div>
+      <div className="cardoffer">  
+         <p>% Offers Available</p>
+       
+        </div>
+        {/* favoritecontainer */}
+        <div className="favoritecontainer">
+        <div className="head1">More From My Favourite</div>
+        </div>
+
+      
     </>
   );
 }
